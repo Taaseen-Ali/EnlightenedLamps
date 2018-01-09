caf775c1b6d38680f0c34e544687b33bea77c2d7
Eddie <hzhu3@stuy.edu>
2018-01-09 07:08:16 -0500

    new issue

---
+++ README.m
@@ -0,0 +1,15 @@
+Enlightened Lamps
+Taaseen Ali, Frank Chen, Eddie Zhu
+
+Crossword Engine 5000
+
+Description:
+This is a program that is used to generate the skeleton of a crossword puzzle. The program can take in a file that contains a list of words to be used to generate the puzzle. The program can also take in a file which is filled with words already sorted in a crossword format. The user can additionally input a file with a list of clues. 
+
+Instructions:
+1) Compile and Run woo.java
+2) Input word file.
+3) Input clue file.
+2) Input a position to obtain a clue for that row/column.
+3) Input a word if the answer is known, another position otherwise. The user can also input one letter at a time. 
+4) Repeat until the crossword is solved. 
